b19ff5959802c1354c37cce03751c8dc8ddd73be
Gilliard Junior <gilliardtorresjuni@hotmail.com>
2024-03-11 19:32:19 -0300

    Exercício 079

--- Estudos/Exercícios/CursoEmVideo/Ex.079.py
+++ Estudos/Exercícios/CursoEmVideo/Ex.079.py
@@ -3,9 +3,9 @@ while True:
     n = int(input('Digite um número: '))
     if n not in valores:
         valores.append(n)
-    resp = str(input('Deseja continuar [S/N]? ')).strip().upper()
+    resp = str(input('Deseja continuar [S/N]? ')).strip().upper()[0]
     if resp not in 'SN':
-         resp = str(input('Resposta inválida, tente novamente. Deseja continuar [S/N]? ')).strip().upper()
+         resp = str(input('Resposta inválida, tente novamente. Deseja continuar [S/N]? ')).strip().upper()[0]
     if resp in 'N':
         break
 print('=' * 35)
