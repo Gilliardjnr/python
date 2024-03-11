45d5ab2eb9202b0aadd2247b76ee19138a4d745e
Gilliard Junior <gilliardtorresjuni@hotmail.com>
2024-03-11 13:37:48 -0300

    Exercício 79

---
+++ Estudos/Exercícios/CursoEmVideo/Ex.079.py
@@ -0,0 +1,13 @@
+valores = list()
+while True:
+    n = int(input('Digite um número: '))
+    if n not in valores:
+        valores.append(n)
+    resp = str(input('Deseja continuar [S/N]? ')).strip().upper()
+    if resp not in 'SN':
+         resp = str(input('Resposta inválida, tente novamente. Deseja continuar [S/N]? ')).strip().upper()
+    if resp in 'N':
+        break
+print('=' * 35)
+valores.sort()
+print(f'Os valores em ordem crescente são: {valores}')
