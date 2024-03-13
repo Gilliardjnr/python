3f196fac38584382e705b88a307e36429ea55b78
Gilliard Junior <gilliardtorresjuni@hotmail.com>
2024-03-13 17:18:10 -0300

    Exercício 085

--- Estudos/Exercícios/CursoEmVideo/Ex.085.py
+++ Estudos/Exercícios/CursoEmVideo/Ex.085.py
@@ -1,16 +1,13 @@
-numeros = list()
-par = list()
-impar = list()
-for c in range(0, 7):
-    n = int(input('Insira um número: '))
-    if n % 2 == 0:
-        par.append(n)
+num = [[], []]
+valor = 0
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}º valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
     else:
-        impar.append(n)
-
-par.sort()
-impar.sort()
-numeros.append(par)
-numeros.append(impar)
-print('=' * 30)
-print(numeros)
+        num[1].append(valor)
+print('-=' * 30)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares digitados foram: {num[0]}')
+print(f'Os valores ímpares digitado foram: {num[1]}')
