10c6ad2fb2f403ed8eb666d1e9a0f9219fda7dc8
Gilliard Junior <gilliardtorresjuni@hotmail.com>
2024-03-13 19:36:29 -0300

    Exercício 089

--- Estudos/Exercícios/CursoEmVideo/Ex.088.py
+++ Estudos/Exercícios/CursoEmVideo/Ex.088.py
@@ -1,5 +1,26 @@
 from random import randint
-jogos = [randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60)]
-quant = int(input('Quantos jogos você deseja gerar? '))
-for c, j in enumerate(jogos):
-    print(f'{jogos[c]}', end=' ')
+from time import sleep
+lista = list()
+jogos = list()
+print('-' * 30)
+print('     JOGA NA MEGASENA     ')
+print('-' * 30)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f' SORTEANDO {quant} JOGOS ', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i + 1}: {l}')
+    sleep(1)
